e63ac6c1e4647cd172b712dd1c2c2a0da8e15ec0
Stephen Chudleigh <stephen.chudleigh@gmail.com>
2021-07-05 19:34:42 -0700

    fix instructions, rename ADiamonds.gif

--- assets/js/conscious-react/components/card_image.jsx
+++ assets/js/conscious-react/components/card_image.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 
-export const CardImage = ({ card }) =>
-  card=='JO' || card=='XJ'
-  ? <img className='card-img' src={`images/${card}.png`} alt={card} />
-  : <img className='card-img' src={`images/${card}.gif`} alt={card} />
+export const CardImage = ({ card }) => {
+  switch(card) {
+    case 'JO':
+    case 'XJ':
+      return <img className='card-img' src={`images/${card}.png`} alt={card} />
+    case 'AD':
+      return <img className='card-img' src={`images/ADiamonds.gif`} alt={card} />
+    default:
+      return <img className='card-img' src={`images/${card}.gif`} alt={card} />
+  }
+}
